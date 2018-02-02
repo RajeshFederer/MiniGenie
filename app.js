941d0c7d8e42e80197636c698d02dcb2f9478ada
Rajeshwaran <rajeshfederer2@gmail.com>
2018-02-02 11:28:20 +0530

    created action.js to seperate logic feom app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const request = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const actions = require('./lib/action');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,30 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) =>{
-
-    if(req.body.result.action === "Welcome"){
-        console.log('I AM IN');
-        let resObj = {
-            speech:'I am a bot',
-            messages: [
-                {
-                  "type": 2,
-                  "platform": "facebook",
-                  "title": "HEY! I'm the Mini Genie Bot. I can help you to create/check incidents",
-                  "replies": [
-                    "Create Incident",
-                    "Check My Incidents"
-                  ]
-                },
-                {
-                  "type": 0,
-                  "speech": "Hey! I'm the Mini Genie Bot. I can help you to create/check incidents. Tell me what I can do for you. Create New Incident(type \"NEW\") Check My Incidents(type \"VIEW\")"
-                }
-              ]
-        }
-        return res.json(resObj);
-    }
-
+    return actions[req.body.result.action](req, res);
 });
 
 

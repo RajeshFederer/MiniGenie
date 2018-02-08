ec458493f55501bac9d5e8a0d85281db2dbe6844
Rajeshwaran <rajeshfederer2@gmail.com>
2018-02-08 10:48:28 +0530

    menu testing

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) =>{
+    console.log('ALL TESTING',JSON.stringify(req.body.result));
     console.log('ACTIOn NAME ', req.body.result.action);
     return actions[req.body.result.action](req, res);
 });

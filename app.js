57c57fb0d8ebdfa4050211ad602eb7638749f2c2
Rajeshwaran <rajeshfederer2@gmail.com>
2018-02-13 14:49:38 +0530

    google assistant first commit

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const request = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-const actions = require('./lib/action');
+const DialogflowApp = require('actions-on-google');
+const facebookActions = require('./lib/facebookAction');
+const googleActionMap = require('./lib/googleAction');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,13 +13,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) =>{
-    console.log('I', req);
-    if(req.object.page.entry.messaging.postback || req.body.object.page.entry.messaging.postback){
-        console.log('1 ',req.object, req.body);
+
+    if(req.body.originalRequest.source == 'google'){
+        
+        const app = new DialogflowApp({ request: request, response: response });
+        console.log('I', req.body.originalRequest.source);
+        app.handleRequest(googleActionMap);
+    } else if(req.body.originalRequest.source == 'facebook'){
+        if(req.object.page.entry.messaging.postback || req.body.object.page.entry.messaging.postback){
+            console.log('1 ',req.object, req.body);
+        }
+        console.log('2 ',JSON.stringify(req.object), JSON.stringify(req.body.object));
+        console.log('ACTIOn NAME ', req.body.result.action);
+        return facebookActions[req.body.result.action](req, res);
     }
-    console.log('2 ',JSON.stringify(req.object), JSON.stringify(req.body.object));
-    console.log('ACTIOn NAME ', req.body.result.action);
-    return actions[req.body.result.action](req, res);
 });
 
 

706022d46095826b0734b80db89cde38f82c08c2
Rajeshwaran <rajeshfederer2@gmail.com>
2018-03-01 13:09:58 +0530

    passporto auth integeration

--- app.js
+++ app.js
@@ -3,18 +3,49 @@
 const request = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const Auth0Strategy = require('passport-auth0');
+const passport = require('passport');
 
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 const facebookActions = require('./lib/facebookAction');
 const slackActions = require('./lib/slackAction');
 const googleActionMap = require('./lib/googleAction').actionMap;
 const googleActionFunctions = require('./lib/googleAction').actionFunctions;
+const data = require('./lib/data/inicdentData');
+
+const accountLinkingToken, redirectURI;
+const strategy = new Auth0Strategy(
+    {
+        domain: data.authO.domain,
+        clientID: data.authO.clientId,
+        clientSecret: data.authO.clientSecret,
+        callbackURL: data.authO.callbackUrl
+    },
+    (accessToken, refreshToken, extraParams, profile, done) => {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+        // extraParams.id_token has the JSON Web Token
+        // profile has all the information from the user
+        return done(null, profile);
+    }
+);
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.post('/', (req, res) =>{
 
@@ -42,6 +73,27 @@ app.post('/', (req, res) =>{
     } else if(req.body.originalRequest.source == 'slack'){
         return slackActions[req.body.result.action](req, res);
     }
+});
+
+app.get('/authorize', passport.authenticate('auth0', {
+    clientID: data.authO.clientId,
+    domain: data.authO.domain,
+    redirectUri: data.authO.callbackUrl,
+    responseType: 'code',
+    audience: 'https://' + data.authO.domain + '/userinfo',
+    scope: 'openid profile'
+}));
+
+app.get('/login', (req, res) => {
+    console.log('Body '+req.body, req.query);
+    accountLinkingToken = req.query.account_linking_token;
+    redirectURI = req.query.redirect_uri;
+    res.redirect('/authorize');
+});
+
+app.get('/callback', passport.authenticate('auth0', {}), (req, res) => {
+    console.log('CALLBACK ' + redirectURI + "&authorization_code=34s4f545");
+    res.redirect(redirectURI +"&authorization_code=34s4f545");
 });
 
 
